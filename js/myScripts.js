b5cc1260e5aa8a7f861930484ff714c1db45e9fd
lubgade <lubgade@yahoo.com>
2017-02-20 18:29:12 -0500

    Add files via upload

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,3 +1,5 @@
+/* This function accesses the modal content to be shown relative to the div */
+
 function myFunction() {
   var my_modal = document.getElementById('modal-inner-content');
   var current_div = event.target.parentElement.parentElement.parentElement.parentElement.lastElementChild;
